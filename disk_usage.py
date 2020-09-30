244f8f2075025e5c0848eeb0e80aa1b583d27407
Ubaydullo <ubuhobbit@gmail.com>
2020-09-30 16:48:07 +0500

    Create a wrapper function for check_disk_full

--- disk_usage.py
+++ disk_usage.py
@@ -1,7 +1,7 @@
+
 import shutil
-
+import os
 import sys
-
 
 def check_disk_usage(disk, min_gb, min_percent):
 	""" 
@@ -11,16 +11,31 @@ def check_disk_usage(disk, min_gb, min_percent):
 	"""
 	du = shutil.disk_usage(disk)
 	percent_free = du.free / du.total * 100
-	print(percent_free)
+	# print(percent_free)
 	gigabytes_free = du.free / 2 ** 30
-	print(gigabytes_free)
+	# print(gigabytes_free)
 	if percent_free < min_percent or gigabytes_free < min_gb:
-		return False
-	return True
+		return True
+	return False
 
-if not check_disk_usage(disk="/", min_gb=2, min_percent=10):
-	print("ERROR: Not enough disk space!")
-	sys.exit(1)
+def check_root_full():
+	""" Returns True if the root partition is full, False otherwse """
+	return check_disk_usage(disk="/", min_gb=2, min_percent=10)
 
-print("Everything ok.")
-sys.exit(0)
+def check_reboot():
+    """ This returns a True if a computer is pending a reboot. """
+    return os.path.exists("/run/reboot-required")
+
+def main():
+    if check_reboot():
+        print("Pending reboot.")
+        sys.exit(1)
+    if check_root_full():
+    	print("Root partition is full.")
+    	sys.exit(1)
+
+    print("Everything is ok!")
+    sys.exit(0)
+
+main()
+
